2ca768c1756025ab3710fb73b41030a02faa02a8
theparsifal21 <162910674+theparsifal21@users.noreply.github.com>
2026-05-15 19:50:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,19 @@
 const cacheName = 'szin-elemzo-v1';
-const assets = ['./', './index.html', './style.css', './script.js'];
+const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
-// Install the service worker and cache files
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Fetch files from cache if offline
+// NEW: This cleans up old versions of your app
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== cacheName).map(key => caches.delete(key)));
+    })
+  );
+});
+
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
